fe8d2b40a9e6e3e6a964de23ea5e1df6a9ebbce3
Mohmmad Amir <76723565+Mohmmad-amir@users.noreply.github.com>
2023-02-23 19:12:13 +0600

    chapter 5 || web server finally complete || Next chapter is express.js

--- node-basic/app.js
+++ node-basic/app.js
@@ -10,15 +10,42 @@ const fsPromises = require('fs').promises;
 const logEvent = require('./logEvent');
 const EventEmitter = require('events');
 const PORT = process.env.PORT || 3500;
-
-
 class Emitter extends EventEmitter { };
-
 const myEmitter = new Emitter();
 
 
+myEmitter.on('log', (msg, filename) => logEvent(msg, filename));
+
+
+
+const serverFile = async (filePath, contentType, response) => {
+
+    try {
+        const rowData = await fsPromises.readFile(
+            filePath,
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rowData) : rowData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404 : 200,
+            { 'Content-Type': contentType }
+        )
+        response.end(
+            contentType === 'application/json' ?
+                JSON.stringify(data) : data
+        )
+    } catch (err) {
+        console.log(err);
+        myEmitter.emit('log', `${err.name}:${err.message}`, 'errLog.txt')
+        response.statusCode = 500;
+        response.end()
+    }
+}
+
 const app = http.createServer((req, res) => {
     console.log(req.url, req.method);
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt')
 
     let path;
     // this is if statement version
@@ -97,20 +124,24 @@ const app = http.createServer((req, res) => {
 
     const fileExists = fs.existsSync(filePath)
     if (fileExists) {
-        //serve
+        serverFile(filePath, contentType, res);
     } else {
         //404
         //301 redirect
-        console.log(Path.parse(filePath));
+        // console.log(Path.parse(filePath));
+        switch (Path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, { location: '/new-page.html ' })
+                res.end;
+                break;
+            case 'www-page.html':
+                res.writeHead(301, { location: '/' })
+                res.end;
+                break;
+            default:
+                // serve 404 response
+                serverFile(Path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
     }
 })
-
-
-
-
-// myEmitter.on('log', (msg) => logEvent(msg));
-
-//     myEmitter.emit('log', 'Log event emitted')
-
-
 app.listen(PORT, () => console.log(`app is running on ${PORT}`))
--- node-basic/logEvent.js
+++ node-basic/logEvent.js
@@ -8,7 +8,7 @@ const fsPromises = require('fs').promises;
 const path = require('path');
 
 
-const logEvent = async (message) => {
+const logEvent = async (message, logName) => {
     const dateTime = `${format(new Date(), "yyyy-MM-dd\tHH:mm:ss")}`
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`
     console.log(logItem);
@@ -16,7 +16,7 @@ const logEvent = async (message) => {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'))
         }
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem)
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem)
     } catch (err) {
         console.log(err);
     }
